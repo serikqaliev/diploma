0aab95859a79de716ea50c446acd827c726504a8
Akzhol <serikkaliev-akzhol@mail.ru>
2022-05-11 19:59:15 +0600

    table changes

--- src/pages/Syntax/SyntaxTable.js
+++ src/pages/Syntax/SyntaxTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Table from "react-bootstrap/Table";
+import sty from "./SyntaxCard.module.css";
 
 const TableComponent = (props) => {
     const words = props.words;
@@ -7,14 +8,20 @@ const TableComponent = (props) => {
     const options = props.options;
 
     return (
-        <div style={{ overflowX: "scroll" }}>
-            <Table striped bordered hover size="sm" responsive="sm" style={{width: "auto", marginLeft: "auto", marginRight: "auto"}}>
+        <div className={sty.wrapperStyle}>s
+            <Table striped bordered hover size="sm" responsive="sm"
+                   style={{width: "auto", marginLeft: "auto", marginRight: "auto"}}>
 
                 <thead>
                 <tr>
                     <td></td>
                     {words.map((word) => (
-                        <th key={word} style={{transform: "rotate(180deg)", writingMode: "vertical-lr", width: "30px", textAlign: "start"}}>
+                        <th key={word} style={{
+                            transform: "rotate(180deg)",
+                            writingMode: "vertical-lr",
+                            width: "30px",
+                            textAlign: "start"
+                        }}>
                             {word}
                         </th>
                     ))}
@@ -25,10 +32,15 @@ const TableComponent = (props) => {
                 {words.map((word, rowIndex) => {
                     return (
                         <tr key={`${word}tr`} style={{textAlign: "end"}}>
-                            <th>{word}</th>
+                            <th className={sty.thStyle} ref={(node) => {
+                                if (node) {
+                                    node.style.setProperty('background-color', 'white', 'important');
+                                }
+                            }}>{word}</th>
                             {words.map((word, colIndex) => {
                                 return (
-                                    <td key={`${word}td`} style={colors.length !== 0 ? {backgroundColor: colors[rowIndex][colIndex]} : {backgroundColor: "white"}}>
+                                    <td key={`${word}td`}
+                                        style={colors.length !== 0 ? {backgroundColor: colors[rowIndex][colIndex]} : {backgroundColor: "white"}}>
                                         <select
                                             defaultValue={"-"}
                                             onChange={(event) =>
