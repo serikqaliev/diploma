656906af528017ed397a8ea3b32c3fd6859b8f68
Akzhol <serikkaliev-akzhol@mail.ru>
2022-05-11 19:51:05 +0600

    first commit

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://86.107.198.41/api';
+export const API_URL = process.env('REACT_APP_API_URL');
 //export const API_URL = `https://aqueous-fjord-78834.herokuapp.com/api`;
 
 const $api = axios.create({
@@ -17,7 +17,7 @@ $api.interceptors.response.use((config) => {
     return config;
 },async (error) => {
     const originalRequest = error.config;
-    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
         originalRequest._isRetry = true;
         try {
             const response = await axios.get(`${API_URL}/user/refresh`, {withCredentials: true})
