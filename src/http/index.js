3e0a6ee665a91335507a21bc5ed35592bd5d3210
Akzhol <serikkaliev-akzhol@mail.ru>
2022-05-12 00:55:53 +0600

    api to server

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5091/api";
+export const API_URL = "http://86.107.198.41/api";
 
 const $api = axios.create({
     withCredentials: true,
