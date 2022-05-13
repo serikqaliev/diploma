4a410651ae3ab87aa54e2465d942d31bf563185c
Akzhol <72283384+serikqaliev@users.noreply.github.com>
2022-05-13 20:14:12 +0600

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://api.text-marking.kz/api";
+export const API_URL = "http://86.107.198.41/api";
 
 const $api = axios.create({
     withCredentials: true,
